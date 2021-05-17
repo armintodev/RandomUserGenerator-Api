b1b718d24b00822b660c59428ff2d586f8c8d57f
arminatwork <workwitharmin7@gmail.com>
2021-05-17 21:31:51 +0430

    feat added client helper utility

---
+++ UserGenerator.Api/Http/ClientHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace UserGenerator.Api.Http
+{
+    internal class ClientHelper : IClientHelper
+    {
+        private readonly string _baseAddress;
+        public ClientHelper(string baseAddress)
+        {
+            _baseAddress = baseAddress;
+        }
+        public async Task<TEntity> Detail<TEntity>(string requestUri)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(_baseAddress)
+            };
+            var detail = await client.GetFromJsonAsync<TEntity>(requestUri).ConfigureAwait(false);
+            return detail;
+        }
+    }
+}
---
+++ UserGenerator.Api/Http/IClientHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace UserGenerator.Api.Http
+{
+    public interface IClientHelper
+    {
+        Task<TEntity> Detail<TEntity>(string requestUri);
+    }
+}
