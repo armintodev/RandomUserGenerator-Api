cf4cfabed355e09deb393c9702a0824349aaef58
arminatwork <workwitharmin7@gmail.com>
2021-05-17 21:31:01 +0430

    feate added send request factory

---
+++ UserGenerator.Api/Http/Request/SendRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using UserGenerator.Api.DTOs;
+
+namespace UserGenerator.Api.Http.Request
+{
+    public class SendRequest
+    {
+        private readonly IClientHelper _client;
+        private readonly string BaseAddress = "https://randomuser.me";
+        private string _requestUri = "/api";
+
+        public SendRequest()
+        {
+            _client = new ClientHelper(BaseAddress);
+        }
+
+        public async Task<CreateUserResult> SendAsync()
+        {
+            return await _client.Detail<CreateUserResult>(_requestUri);
+        }
+
+        public async Task<CreateUserResult> SendAsync(string param)
+        {
+            return await _client.Detail<CreateUserResult>(_requestUri += param);
+        }
+    }
+}
