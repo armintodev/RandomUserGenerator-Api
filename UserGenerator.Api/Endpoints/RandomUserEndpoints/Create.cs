a38c1c6db5500f1f28370f5d2b888493c1ef6f14
arminatwork <workwitharmin7@gmail.com>
2021-05-17 21:33:34 +0430

    feat added create user endpoints

---
+++ UserGenerator.Api/Endpoints/RandomUserEndpoints/Create.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+using UserGenerator.Api.DTOs;
+using UserGenerator.Api.Http.Request;
+
+namespace UserGenerator.Api.Endpoints.RandomUserEndpoints
+{
+    public class Create : BaseAsyncEndpoint.WithRequest<CreateUserCommand>.WithResponse<CreateUserResult>
+    {
+        [HttpPost("user")]
+        public override async Task<ActionResult<CreateUserResult>> HandleAsync(CreateUserCommand request, CancellationToken cancellationToken = new())
+        {
+            //TODO: request to https://randomuser.me and get detail. second save to database
+            //client
+            var client = new SendRequest();
+
+            CreateUserResult response;
+
+            if (!request.Request.Equals(""))
+                response = await client.SendAsync(request.Request);
+            else
+                response = await client.SendAsync();
+
+            return response;
+        }
+    }
+}
