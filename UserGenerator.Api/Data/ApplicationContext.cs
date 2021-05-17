bba5a52723d40fa17a63f3239d6f87611ac0970a
arminatwork <workwitharmin7@gmail.com>
2021-05-17 21:29:16 +0430

    feat added identity db context

---
+++ UserGenerator.Api/Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserGenerator.Api.Data
+{
+    public class ApplicationContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
