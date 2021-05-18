95d896b08f2c2bf298cae71f31801f583eefeeb7
arminatwork <workwitharmin7@gmail.com>
2021-05-18 23:54:19 +0430

    feat added action context extension

---
+++ UserGenerator.Api/Utilities/AppResult/ActionContextExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserGenerator.Api.Utilities.AppResult
+{
+    public static class ActionContextExtensions
+    {
+        public static Task Ok(this ActionContext context, object value)
+        {
+            return new OkObjectResult(value).ExecuteResultAsync(context);
+        }
+
+        public static Task UnProcessableEntity(this ActionContext context, string error)
+        {
+            return new UnprocessableEntityObjectResult(error).ExecuteResultAsync(context);
+        }
+    }
+}
