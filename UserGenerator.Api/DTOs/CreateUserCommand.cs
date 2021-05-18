896862cd6282a8656dd73e19b8e9b4e4ef65a78b
arminatwork <workwitharmin7@gmail.com>
2021-05-19 00:37:39 +0430

    refactor added missing property

--- UserGenerator.Api/DTOs/CreateUserCommand.cs
+++ UserGenerator.Api/DTOs/CreateUserCommand.cs
@@ -1,4 +1,9 @@
 ﻿namespace UserGenerator.Api.DTOs
 {
-    public record CreateUserCommand(string Request);
+    public record CreateUserCommand
+    {
+        public string UserName { get; init; }
+        public string Email { get; init; }
+        public string Request { get; init; }
+    };
 }
