bc8c63ac78f4b9e315a3c4b63e6d33cc876c8937
arminatwork <workwitharmin7@gmail.com>
2021-05-18 23:54:37 +0430

    feat added api result

---
+++ UserGenerator.Api/Utilities/AppResult/ApiResult.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserGenerator.Api.Utilities.AppResult
+{
+    public sealed class ApiResult : IActionResult
+    {
+        private readonly IResult _result;
+        public ApiResult(IResult result)
+        {
+            _result = result;
+        }
+
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            return _result.Failed ? context.UnProcessableEntity(_result.Message) : context.Ok(_result.Message);
+        }
+    }
+
+    public sealed class ApiResult<TData> : IActionResult
+    {
+        private readonly IResult<TData> _result;
+        public ApiResult(IResult<TData> result)
+        {
+            _result = result;
+        }
+
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            return _result.Failed ? context.UnProcessableEntity(_result.Message) : context.Ok(_result.Data);
+        }
+    }
+}
