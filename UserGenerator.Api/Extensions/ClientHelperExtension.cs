96b1a98cfbae8ef0fc787bd8378990cebdcbf9eb
arminatwork <workwitharmin7@gmail.com>
2021-05-19 00:40:39 +0430

    feat added client helper extension

---
+++ UserGenerator.Api/Extensions/ClientHelperExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UserGenerator.Api.Http;
+
+namespace UserGenerator.Api.Extensions
+{
+    public static class ClientHelperExtension
+    {
+        public static void AddClientHelper(this IServiceCollection services)
+        {
+            services.AddScoped<IClientHelper, ClientHelper>();
+        }
+    }
+}
