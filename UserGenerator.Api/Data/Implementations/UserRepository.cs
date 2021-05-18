41d5403259e07f2c33a62609a88ea268927adfa5
arminatwork <workwitharmin7@gmail.com>
2021-05-18 23:57:21 +0430

    feat added user repository, insert get, get by query behavior

---
+++ UserGenerator.Api/Data/Implementations/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UserGenerator.Api.Data.Repositories;
+using UserGenerator.Api.DTOs;
+using UserGenerator.Api.Utilities.AppResult;
+
+namespace UserGenerator.Api.Data.Implementations
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationContext _context;
+        public UserRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CreateUserResult>> Get()
+        {
+            return await _context.Users.AsNoTracking().Select(sel => new CreateUserResult(sel.UserName, sel.Email)).ToListAsync();
+        }
+
+        public async Task<List<CreateUserResult>> Get(int query)
+        {
+            var list = _context.Users.Select(sel => new CreateUserResult(sel.UserName, sel.Email)).Take(query);
+            return await list.ToListAsync();
+    }
+
+    public Task<TReturn> Get<TReturn>(string key)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<ApiResult<CreateUserResult>> Add(CreateUserCommand command)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+}
---
+++ UserGenerator.Api/Data/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UserGenerator.Api.DTOs;
+using UserGenerator.Api.Utilities.AppResult;
+
+namespace UserGenerator.Api.Data.Repositories
+{
+    interface IUserRepository
+    {
+        Task<List<CreateUserResult>> Get();
+        Task<List<CreateUserResult>> Get(int query);
+        Task<TReturn> Get<TReturn>(string key);
+        Task<ApiResult<CreateUserResult>> Add(CreateUserCommand command);
+    }
+}
