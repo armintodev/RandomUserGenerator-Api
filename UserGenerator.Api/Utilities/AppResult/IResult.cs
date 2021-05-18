7569cf77e1b655ab53c9d4f60dcd5e6f7d4c8e90
arminatwork <workwitharmin7@gmail.com>
2021-05-18 23:54:54 +0430

    feat added result abstraction

---
+++ UserGenerator.Api/Utilities/AppResult/IResult.cs
@@ -0,0 +1,14 @@
+﻿namespace UserGenerator.Api.Utilities.AppResult
+{
+    public interface IResult
+    {
+        bool Failed { get; }
+        string Message { get; }
+        bool Succeeded { get; }
+    }
+
+    public interface IResult<out TData> : IResult
+    {
+        TData Data { get; }
+    }
+}
