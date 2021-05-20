028b10a7fce994a3954acd7d94442d8acaaa086f
arminatwork <workwitharmin7@gmail.com>
2021-05-21 01:33:01 +0430

    refactor added application context,application identity,application life cycles,fix bugs

--- UserGenerator.Api/Startup.cs
+++ UserGenerator.Api/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using UserGenerator.Api.Extensions;
 
 namespace UserGenerator.Api
 {
@@ -16,18 +17,23 @@ namespace UserGenerator.Api
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
+
+            services.AddApplicationContext(Configuration.GetConnectionString("UserGeneratorConnection"));
+
+            services.AddApplicationIdentity();
+
+            services.AddApplicationLifeCycles();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "UserGenerator.Api", Version = "v1" });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
