bdd243900caaeb8d40d446025c349821b053fdbe
arminatwork <workwitharmin7@gmail.com>
2021-05-21 00:38:57 +0430

    refactor application db context ignore dont need tables and property user

--- UserGenerator.Api/Data/ApplicationContext.cs
+++ UserGenerator.Api/Data/ApplicationContext.cs
@@ -4,11 +4,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace UserGenerator.Api.Data
 {
-    public class ApplicationContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
+    public class ApplicationContext : IdentityDbContext
     {
         public ApplicationContext(DbContextOptions options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUser>().Ignore(ig => ig.AccessFailedCount)
+                .Ignore(ig => ig.ConcurrencyStamp)
+                .Ignore(ig => ig.NormalizedUserName)
+                .Ignore(ig => ig.LockoutEnabled)
+                .Ignore(ig => ig.LockoutEnd)
+                .Ignore(ig => ig.SecurityStamp)
+                .Ignore(ig => ig.TwoFactorEnabled);
+
+            builder.Ignore<IdentityUserLogin<string>>();
+            builder.Ignore<IdentityUserClaim<string>>();
+            builder.Ignore<IdentityUserToken<string>>();
+            builder.Ignore<IdentityRoleClaim<string>>();
         }
     }
 }
