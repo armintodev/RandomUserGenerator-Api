d3e319db4736327c45b7b9891026613f81178fea
arminatwork <workwitharmin7@gmail.com>
2021-05-21 00:40:52 +0430

    refactor changed web service api website

--- UserGenerator.Api/Http/Request/SendRequest.cs
+++ UserGenerator.Api/Http/Request/SendRequest.cs
@@ -6,8 +6,8 @@ namespace UserGenerator.Api.Http.Request
     public class SendRequest
     {
         private readonly IClientHelper _client;
-        private readonly string BaseAddress = "https://randomuser.me";
-        private string _requestUri = "/api";
+        private readonly string BaseAddress = "https://jsonplaceholder.typicode.com";
+        private string _requestUri = "/users/";
 
         public SendRequest()
         {
@@ -19,7 +19,7 @@ namespace UserGenerator.Api.Http.Request
             return await _client.Detail<CreateUserResult>(_requestUri);
         }
 
-        public async Task<CreateUserResult> SendAsync(string param)
+        public async Task<CreateUserResult> SendAsync(int param)
         {
             return await _client.Detail<CreateUserResult>(_requestUri += param);
         }
